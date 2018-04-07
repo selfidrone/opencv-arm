1534527021b5ff0cfd9d10c82751e7ed45612e0a
Nic Jackson <jackson.nic@gmail.com>
2018-04-07 14:38:59 +0000

    Added build file

---
+++ buildOpenCV.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+version=3.4.0
+
+# UPDATE APT
+sudo apt-get update
+
+# INSTALL DEPS
+sudo apt-get install -y build-essential cmake pkg-config
+sudo apt-get install -y libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
+sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get install -y libxvidcore-dev libx264-dev
+sudo apt-get install -y libgtk2.0-dev
+sudo apt-get install -y libatlas-base-dev gfortran
+
+# FETCH SOURCE
+wget -O opencv.zip https://github.com/opencv/opencv/archive/${version}.zip
+wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/${version}.zip
+unzip opencv_contrib.zip
+unzip opencv.zip
+
+# CREATE BUILD FOLDER
+cd opencv-${version}
+mkdir build && cd build
+
+# PREPARE CONFIG
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+      -D CMAKE_INSTALL_PREFIX=/usr/local \
+      -D INSTALL_PYTHON_EXAMPLES=ON \
+      -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-${version}/modules \
+      -D BUILD_EXAMPLES=ON \
+      -D ENABLE_PRECOMPILED_HEADERS=OFF ..
+
+# BUILD LIBRARY USING 4 CORES
+make -j4
+
+## INSTALL
+#sudo make install
+#sudo ldconfig
